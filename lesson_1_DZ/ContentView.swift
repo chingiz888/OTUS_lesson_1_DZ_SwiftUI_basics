c42316c5cb9fa818be7567789ec469104d992fae
Chingiz <chingiz888@mail.ru>
2020-11-29 21:08:04 +0300

    Initial Commit

---
+++ lesson_1_DZ/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lesson_1_DZ
+//
+//  Created by super_comp on 29.11.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
